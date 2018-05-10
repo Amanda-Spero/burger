660811e3a639319fb324f33f2caa4b1ccbb269d1
Amanda Helms <amandaspero@Amandas-MacBook-Pro.local>
2018-05-10 17:36:54 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("hypocrite", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("classic", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cow_town", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("wd-40", true);
